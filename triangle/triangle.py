b3aea0a8dd8ecbd28cf3cea44de0e9df4c5a9bfd
kalkidan12 <kalget12@gmail.com>
2023-01-16 20:14:48 +0300

    triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,34 @@
 def equilateral(sides):
-    pass
+    for i in sides:
+        if(i <= 0 ):
+            return False
+    if((sides[0]+sides[1]<sides[2]) or (sides[1]+sides[2]<sides[0]) or (sides[0]+sides[2]<sides[1])):
+        return False
+    if(sides[0]==sides[1]==sides[2]):
+        return True
+    else:
+        return False
 
 
 def isosceles(sides):
-    pass
+    for i in sides:
+        if(i<= 0 ):
+            return False
+    if((sides[0]+sides[1]<sides[2]) or (sides[1]+sides[2]<sides[0]) or (sides[0]+sides[2]<sides[1])):
+        return False
+    elif(sides[0] == sides[1] or sides[1]==sides[2] or sides[0]==sides[2]):
+        return True
+    else:
+        return False
 
 
 def scalene(sides):
-    pass
+    for i in sides:
+        if(i <= 0 ):
+            return False
+    if((sides[0]+sides[1]<sides[2]) or (sides[1]+sides[2]<sides[0]) or (sides[0]+sides[2]<sides[1])):
+        return False
+    elif(sides[0] != sides[1] and sides[1] != sides[2] and sides[0]!=sides[2]):
+        return True
+    else:
+        return False
